ab524e8aca4098863e58fe3da726e3836ab45bf5
martin <martin@within.co.kr>
2017-09-03 14:31:38 +0900

    Vuejs를 이용한 Todo App

---
+++ src/Todo/App.vue
@@ -0,0 +1,114 @@
+<template>
+  <div id="app">
+    <div class="todoapp">
+      <app-header
+				@addTodo="addTodo"
+			/>
+			<todo-list
+				:todos="todos"
+			/>
+      <app-footer/>
+    </div>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/Header.vue';
+import AppFooter from './components/Footer.vue';
+import TodoList from './components/TodoList.vue';
+export default {
+  name: 'app',
+  data () {
+    return {
+			todos: [
+					{
+							key: 0,
+							name: '투두앱1',
+							isDone: true
+					},{
+							key: 1,
+							name: '투두앱2',
+							isDone: false
+					},{
+							key: 2,
+							name: '투두앱3',
+							isDone: false
+					}
+			]
+    }
+	},
+	methods: {
+		addTodo (text) {
+			const name = text;
+			const key = Date.now();
+			const isDone = false;
+			this.todos = [...this.todos, { key, name, isDone }];
+		}
+	},
+  components: {
+    AppHeader,
+		AppFooter,
+		TodoList
+  }
+}
+</script>
+
+<style lang="scss">
+
+body {
+	margin: 0;
+	padding: 0;
+}
+
+button {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	background: none;
+	font-size: 100%;
+	vertical-align: baseline;
+	font-family: inherit;
+	font-weight: inherit;
+	color: inherit;
+	-webkit-appearance: none;
+	appearance: none;
+	-webkit-font-smoothing: antialiased;
+	-moz-font-smoothing: antialiased;
+	font-smoothing: antialiased;
+}
+
+body {
+	font: 14px 'Helvetica Neue', Helvetica, Arial, sans-serif;
+	line-height: 1.4em;
+	background: #f5f5f5;
+	color: #4d4d4d;
+	min-width: 230px;
+	max-width: 550px;
+	margin: 0 auto;
+	-webkit-font-smoothing: antialiased;
+	-moz-font-smoothing: antialiased;
+	font-smoothing: antialiased;
+	font-weight: 300;
+}
+
+button,
+input[type="checkbox"] {
+	outline: none;
+}
+
+.hidden {
+	display: none;
+}
+
+.todoapp {
+	background: #fff;
+	margin: 130px 0 40px 0;
+	position: relative;
+	box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2),
+	            0 25px 50px 0 rgba(0, 0, 0, 0.1);
+}
+label{
+	margin:0;
+	padding:0;
+}
+</style>
---
+++ src/Todo/components/Footer.vue
@@ -0,0 +1,119 @@
+<template>
+<div>
+    <footer class="footer">
+        <span class="todo-count">
+            <strong>1</strong> item left
+        </span> 
+        <ul class="filters">
+            <li><a href="#/all" class="selected">All</a></li>
+            <li><a href="#/active" class="">Active</a></li>
+            <li><a href="#/completed" class="">Completed</a></li>
+        </ul> 
+        <button class="clear-completed" style="display: none;">Clear completed </button>
+    </footer>
+</div>
+</template>
+<script>
+export default{
+    name: 'Footer'
+}
+</script>
+<style lang="scss" scoped>
+
+.footer {
+	color: #777;
+	padding: 10px 15px;
+	height: 20px;
+	text-align: center;
+	border-top: 1px solid #e6e6e6;
+    .todo-count {
+        float: left;
+        text-align: left;
+        strong {
+            font-weight: 300;
+        }
+    }
+    .filters {
+        margin: 0;
+        padding: 0;
+        list-style: none;
+        position: absolute;
+        right: 0;
+        left: 0;
+        li {
+            display: inline;
+            a {
+                color: inherit;
+                margin: 3px;
+                padding: 3px 7px;
+                text-decoration: none;
+                border: 1px solid transparent;
+                border-radius: 3px;
+                &.selected{
+                    border-color: rgba(175, 47, 47, 0.2);
+                }
+                &:hover{
+                border-color: rgba(175, 47, 47, 0.1);
+                }
+            }
+        }
+    }
+    .clear-completed {
+        float: right;
+        position: relative;
+        line-height: 20px;
+        text-decoration: none;
+        cursor: pointer;
+        position: relative;
+        &:hover{
+            text-decoration: underline;
+        }
+        &:active{
+            
+        }
+    }
+    &:before{
+        content: '';
+            position: absolute;
+            right: 0;
+            bottom: 0;
+            left: 0;
+            height: 50px;
+            overflow: hidden;
+            box-shadow: 0 1px 1px rgba(0, 0, 0, 0.2),
+                        0 8px 0 -3px #f6f6f6,
+                        0 9px 1px -3px rgba(0, 0, 0, 0.2),
+                        0 16px 0 -6px #f6f6f6,
+                        0 17px 2px -6px rgba(0, 0, 0, 0.2);
+    }
+}
+
+@media screen and (-webkit-min-device-pixel-ratio:0) {
+	.toggle-all,
+	.todo-list li .toggle {
+		background: none;
+	}
+
+	.todo-list li .toggle {
+		height: 40px;
+	}
+
+	.toggle-all {
+		-webkit-transform: rotate(90deg);
+		transform: rotate(90deg);
+		-webkit-appearance: none;
+		appearance: none;
+	}
+}
+
+@media (max-width: 430px) {
+	.footer {
+		height: 50px;
+	}
+
+	.filters {
+		bottom: 10px;
+	}
+}
+
+</style>
