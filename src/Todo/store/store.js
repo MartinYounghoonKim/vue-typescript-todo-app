34e6efad96282db2f272ea1dae63d2f12db174a2
younghoon <hoons0131@gmail.com>
2017-09-27 22:44:45 +0900

    Seperate mutation and state

--- src/Todo/store/store.js
+++ src/Todo/store/store.js
@@ -1,37 +1,39 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import axios from 'axios';
 
 import getters from './getters.js';
 import actions from './actions.js';
 
-import TodoApi from '../api/api_core';
 import TODO from '../constant/mutation-type';
-
 
 Vue.use(Vuex);
 
+const state = {
+    todos: []
+};
+
+const mutations = {
+  [TODO.ADD] (state, payload){
+    state.todos = [...state.todos, payload];
+  },
+  [TODO.EDIT] (state, payload) {
+    const targetKey = payload.id;
+    const editedTodo = payload.editedTodo;
+        
+    state.todos.splice( state.todos.findIndex(v => v.id === targetKey), 1, payload);
+  },
+  [TODO.DELETE] (state, deleteTargetKey) {
+    state.todos.splice(deleteTargetKey, 1);
+  },
+  [TODO.ALL_COMPLETE] (state, payload) {
+    state.todos = payload.map( v=>v.data );
+  }
+}
+
+
 export const store = new Vuex.Store({
-    state: {
-        todos: []
-    },
+    state,
     getters,
-    mutations: {
-        [TODO.ADD] (state, payload){
-            state.todos = [...state.todos, payload];
-        },
-        [TODO.EDIT] (state, payload) {
-            const targetKey = payload.id;
-            const editedTodo = payload.editedTodo;
-            
-            state.todos.splice( state.todos.findIndex(v => v.id === targetKey), 1, payload);
-        },
-        [TODO.DELETE] (state, deleteTargetKey) {
-            state.todos.splice(deleteTargetKey, 1);
-        },
-        [TODO.ALL_COMPLETE] (state, payload) {
-            state.todos = payload.map( v=>v.data );
-        }
-    },
+    mutations,
     actions
 });
