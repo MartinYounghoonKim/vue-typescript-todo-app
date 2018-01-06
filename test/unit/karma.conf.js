ea0e9e990edb414c76003ff1834a4d764e953413
martinyounghoonkim <hoons0131@gmail.com>
2018-01-06 13:38:38 +0900

    Add specs directory for unit test

---
+++ test/unit/karma.conf.js
@@ -0,0 +1,27 @@
+var webpackConfig = require('../../build/webpack.test.conf')
+
+module.exports = function (config) {
+  config.set({
+    browsers: ['PhantomJS'],
+    frameworks: ['mocha', 'sinon-chai', 'phantomjs-shim'],
+    reporters: ['spec', 'coverage'],
+    files: [
+      '../../node_modules/babel-polyfill/dist/polyfill.js',
+      './index.js'
+    ],
+    preprocessors: {
+      './index.js': ['webpack', 'sourcemap']
+    },
+    webpack: webpackConfig,
+    webpackMiddleware: {
+      noInfo: true
+    },
+    coverageReporter: {
+      dir: './coverage',
+      reporters: [
+        { type: 'lcov', subdir: '.' },
+        { type: 'text-summary' }
+      ]
+    }
+  })
+}
---
+++ test/unit/specs/AppHeader.spec.js
@@ -0,0 +1,26 @@
+import { mount } from 'avoriaz';
+
+import Vue from 'vue';
+import Header from '@/components/Header';
+
+describe('Header.vue', () => {
+  it('Should render correct contents with header class attributes', () => {
+    const HeaderComponent = mount(Header);
+    const headerDom = HeaderComponent.find('div')[0];
+
+    expect(headerDom.is('.header'))
+    .to.equal(true);
+  });
+  it('Adds a new todo item to list on enter', () => {
+    const HeaderComponent = mount(Header);
+
+    HeaderComponent.setData({
+      newItem: 'test3'
+    });
+
+    const input = HeaderComponent.find('input')[0];
+    input.trigger('keydown.enter');
+
+    expect(HeaderComponent.data().items).to.contain('test3');
+  });
+})
